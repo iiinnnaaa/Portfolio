2ba24bc11a0190e792f144b61b136d8a46bbf991
iiinnnaaa <isoyan.inn@gmail.com>
2021-03-11 18:43:46 +0400

    worked on dot-navbar

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 function menuFunction() {
 	let getel = document.getElementsByClassName("mobile-menu-icon");
 	getel[0].classList.toggle('clicked');
@@ -13,22 +12,43 @@ function menuFunction() {
 	}
 }
 
-function scrollFunction(id) {
-	let winheight = 7 * window.innerHeight;
-	let calc = winheight * ((id-1)/7);
-	let fin = calc - 60;
-	if(id === 1){
-		window.scrollTo({
-			top: 0,
-			behavior: "smooth",
-		})
+function scrollFunction(id, event) {
+		addClass(id, event);
+
+		let winheight = 7 * window.innerHeight;
+		let calc = winheight * ((id - 1) / 7);
+		let fin = calc + 3;
+		if (id === 1) {
+			window.scrollTo({
+				top: 0,
+				behavior: "smooth",
+			})
+		} else {
+			window.scrollTo({
+				top: fin,
+				behavior: "smooth",
+			})
+		}
 	}
-	else{
-		window.scrollTo({
-			top: fin,
-			behavior: "smooth",
-		})
+
+	function addClass(id, event){
+		let activeLink = document.getElementsByClassName('dot-active');
+
+		if(event.target.isSameNode(activeLink[0])){
+			return;
+		}
+
+		let dotLinks = document.getElementsByClassName('dot-link');
+
+		for (let i = 0; i < dotLinks.length; i++) {
+			if(!event.target.isSameNode(dotLinks[i]) && dotLinks[i].classList.contains('dot-active')) {
+				dotLinks[i].classList.remove('dot-active');
+			}
+
+			else {
+				if (event.target.isSameNode(dotLinks[i])) {
+					event.target.classList.add('dot-active');
+				}
+			}
+		}
 	}
-	 console.log(winheight);
-	console.log(fin);
-}
