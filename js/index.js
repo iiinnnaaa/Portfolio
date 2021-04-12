a9b0b0079548e2d487bec2e225fe95ed2a628f8f
iiinnnaaa <isoyan.inn@gmail.com>
2021-04-12 17:10:12 +0400

    some changes on js file

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
-$(window).on("scroll", function() {
+$(window).on("scroll", function () {
 	let header = document.getElementById('header');
-	if($(window).scrollTop() > 500) {
+	if ($(window).scrollTop() > 500) {
 		header.style.background = "#000000";
-		// header.style.background = "transparent";
 	} else {
 		header.style.background = "transparent";
 	}
@@ -10,35 +9,32 @@ $(window).on("scroll", function() {
 
 
 let menuItems = document.querySelectorAll('.menu-links');
-for(let i = 0; i < menuItems.length; i++){
+for (let i = 0; i < menuItems.length; i++) {
 	menuItems[i].addEventListener("click", menuFunction);
 }
 
 
-window.onscroll = function() {
+window.onscroll = function () {
 	changeNavColor();
 	scrollFunction2();
 };
 
 
 function scrollFunction2() {
-		let nav = document.querySelectorAll(".dot-nav a");
-		let active = document.querySelector(".dot-active");
-		for(let i = 0; i < nav.length; i++){
-			if ( document.documentElement.scrollTop > 2600 && document.documentElement.scrollTop < 4200) {
+	let nav = document.querySelectorAll(".dot-nav a");
+	let active = document.querySelector(".dot-active");
+	for (let i = 0; i < nav.length; i++) {
+		if (document.documentElement.scrollTop > 2600 && document.documentElement.scrollTop < 4200) {
 			nav[i].style.backgroundColor = "#00000055";
 			nav[i].style.borderColor = "#00000055";
 			active.classList.add('dot-active-before');
-		 // document.styleSheets[0].addRule('.dot-active::before','border: 1px solid #00000055');
 			active.style.backgroundColor = "#00000055";
+		} else {
+			nav[i].style.backgroundColor = "#ffffff5c";
+			nav[i].style.borderColor = "#ffffff5c";
+			active.classList.remove('dot-active-before');
+			active.style.backgroundColor = "#ffffff5c";
 		}
-			else{
-				nav[i].style.backgroundColor = "#ffffff5c";
-				active.classList.remove('dot-active-before');
-				nav[i].style.borderColor = "#ffffff5c";
-				// document.styleSheets[0].addRule('.dot-active::before','border: 1px solid #ffffff5c');
-				active.style.backgroundColor = "#ffffff5c";
-			}
 	}
 }
 
@@ -47,8 +43,8 @@ $('.works-slider').slick({
 	dots: false,
 	prevArrow: '.work-left-arrow',
 	nextArrow: '.work-right-arrow',
-	infinite:       true,
-	slidesToShow:   4,
+	infinite: true,
+	slidesToShow: 4,
 	slidesToScroll: 1,
 	responsive: [
 		{
@@ -150,132 +146,70 @@ function menuFunction() {
 	}
 }
 
+let getDotNavItems = document.getElementsByClassName('dot-link');
+for (let i = 0; i < getDotNavItems.length; i++) {
+	getDotNavItems[i].addEventListener("click", scrollFunction);
+}
 
 let getMenuLinkItems = document.getElementsByClassName('menu-links');
-getMenuLinkItems[0].addEventListener("click", scrollFunction);
+
+for (let i = 0; i < getMenuLinkItems.length; i++) {
+	getMenuLinkItems[i].addEventListener("click", scrollFunction);
+}
 
 function scrollFunction(event) {
-		// addClass(event);
-
 	let itTop = document.querySelectorAll('.menu-links a');
 
 	let item;
-	for(let i = 0; i < itTop.length; i++){
-		item = itTop[i].dataset.top;
-	}
-
-	console.log(item);
+	for (let i = 0; i < itTop.length; i++) {
+		item = itTop[i];
+		let data = event.target;
+		let top = data.dataset.top;
 
 		window.scrollTo({
-			top: item,
+			top: top,
 			behavior: "smooth",
 		});
-
-		// let windowHeight = 7 * window.innerHeight;
-		// let calc = windowHeight * ((id - 1) / 5.1);
-		// let fin = calc + 3;
-	// if (id === 1) {
-	// 		window.scrollTo({
-	// 			top: 0,
-	// 			behavior: "smooth",
-	// 		});
-	// 	}
-	// if (id === 2) {
-	// 	window.scrollTo({
-	// 		top: 842,
-	// 		behavior: "smooth",
-	// 	});
-	// }
-	// if (id === 3) {
-	// 	window.scrollTo({
-	// 		top: 1905,
-	// 		behavior: "smooth",
-	// 	})
-	// }
-	// if (id === 4) {
-	// 	window.scrollTo({
-	// 		top: 2919,
-	// 		behavior: "smooth",
-	// 	})
-	// }
-	// if (id === 5) {
-	// 	window.scrollTo({
-	// 		top: 4400,
-	// 		behavior: "smooth",
-	// 	})
-	// }
-	// if (id === 6) {
-	// 	window.scrollTo({
-	// 		top: 5264,
-	// 		behavior: "smooth",
-	// 	})
-	// }
 	}
+}
 
 
-function changeNavColor(){
+function changeNavColor() {
 	let nav = document.querySelectorAll(".dot-nav a");
-		if ( document.documentElement.scrollTop < 832){
-			nav[0].classList.add('dot-active');
-		}
-		else{
-			nav[0].classList.remove('dot-active');
-		}
-		if (document.documentElement.scrollTop > 832 && document.documentElement.scrollTop < 1895){
-		nav[1].classList.add('dot-active');
-	 }
-	 else{
-		nav[1].classList.remove('dot-active');
-	 }
-
-	if (document.documentElement.scrollTop > 1895 && document.documentElement.scrollTop < 2909){
-		nav[2].classList.add('dot-active');
+	if (document.documentElement.scrollTop < 832) {
+		nav[0].classList.add('dot-active');
+	} else {
+		nav[0].classList.remove('dot-active');
 	}
-	else{
+	if (document.documentElement.scrollTop > 832 && document.documentElement.scrollTop < 1895) {
+		nav[1].classList.add('dot-active');
+	} else {
+		nav[1].classList.remove('dot-active');
+	}
+
+	if (document.documentElement.scrollTop > 1895 && document.documentElement.scrollTop < 2909) {
+		nav[2].classList.add('dot-active');
+	} else {
 		nav[2].classList.remove('dot-active');
 	}
-	if (document.documentElement.scrollTop > 2909 && document.documentElement.scrollTop < 4390){
+	if (document.documentElement.scrollTop > 2909 && document.documentElement.scrollTop < 4390) {
 		nav[3].classList.add('dot-active');
-	}
-	else{
+	} else {
 		nav[3].classList.remove('dot-active');
 	}
 
-	if (document.documentElement.scrollTop > 4390 && document.documentElement.scrollTop < 5254){
+	if (document.documentElement.scrollTop > 4390 && document.documentElement.scrollTop < 5254) {
 		nav[4].classList.add('dot-active');
-	}
-	else{
+	} else {
 		nav[4].classList.remove('dot-active');
 	}
-	if (document.documentElement.scrollTop > 5254){
+	if (document.documentElement.scrollTop > 5254) {
 		nav[5].classList.add('dot-active');
-	}
-	else{
+	} else {
 		nav[5].classList.remove('dot-active');
 	}
 }
 
 
-function addClass(event){
-		let activeLink = document.getElementsByClassName('dot-active');
-
-		if(event.target.isSameNode(activeLink[0])){
-			return;
-		}
-
-		let dotLinks = document.getElementsByClassName('dot-link');
-
-		for (let i = 0; i < dotLinks.length; i++) {
-			if(!event.target.isSameNode(dotLinks[i]) && dotLinks[i].classList.contains('dot-active')) {
-				dotLinks[i].classList.remove('dot-active');
-			}
-
-			else {
-				if (event.target.isSameNode(dotLinks[i])) {
-					event.target.classList.add('dot-active');
-				}
-			}
-		}
-	}
 
 
