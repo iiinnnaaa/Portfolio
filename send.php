d157d1d11f126c083ad0a9d52888e2688158f93c
iiinnnaaa <isoyan.inn@gmail.com>
2021-04-12 19:14:35 +0400

    working on adding messages to db

--- send.php
+++ send.php
@@ -1,1 +1,22 @@
 <?php
+//// Connection
+//
+//$host = "localhost";
+//$dbname = "portfolio";
+//$user = "root";
+//$password = "root";
+//
+//$connection = new mysqli($host, $user, $password, $dbname);
+//
+//
+//// Add to db
+//
+//if (isset($_POST['send'])) {
+//  $full_name = $_POST['full-name'];
+//  $email = $_POST['email'];
+//  $subject = $_POST['subject'];
+//  $message = $_POST['message'];
+//}
+//
+//$insert = "INSERT INTO `messages`(`full-name`, `email`, `subject`, `message`)
+//                                       VALUES ('$full_name', '$email', '$subject', '$message')";
